af83d42b239a225e7aee3672ea78fcc09edf0c05
Camille Emmer <camilleemmer@gmail.com>
2022-07-25 12:26:16 -0600

    commit tree 06c0dacd366e7351dd01481c8ac17ffc5b940544

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+const input = document.getElementById('input')
 function addingEventListener() {
+    
+
+    // input.addEventListener('click', function(e){
+    //   return 'I was clicked!'
+    // });
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
+     
+
 }
+
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert); 
+// }
+
+
+
+
+
